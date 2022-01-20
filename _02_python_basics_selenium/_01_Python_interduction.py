e700ef8f870320e6d42c823db71d313ac4ea1fde
Your Name <satish@gmail.com>
2022-01-20 16:54:27 +0530

    20-01-2022

--- _02_python_basics_selenium/_01_Python_interduction.py
+++ _02_python_basics_selenium/_01_Python_interduction.py
@@ -1,1 +1,111 @@
- 
+"""
+Python for Testers
+
+1.Python is a high-level object-oriented programming language developed
+in the late 1980s by Guido van Rossum.
+2.Python is very popular,free and very widely used.
+3.Python is interpreted.
+4.Python is sample and easy to learn.
+
+"""
+# Python 3 program to counts Palindromic
+# Subsequence in a given String using recursion
+
+str = "S,N"
+
+# Function return the total
+# palindromic subsequence
+
+
+def countPS(N, S):
+
+    if(N > S):
+        return 0
+
+    if(dp[N][S] != -1):
+        return dp[N][S]
+
+    if(N == S):
+        dp[N][S] = 1
+        return dp[N][S]
+
+    elif (str[N] == str[S]):
+        dp[N][S] = (countPS(N + 1, S) +
+                    countPS(N, S - 1) + 1)
+        return dp[N][S]
+    else:
+        dp[N][S] = (countPS(N+ 1, S) +
+                    countPS(N, S - 1) -
+                    countPS(N + 1, N - 1))
+        return dp[N][S]
+
+
+# Driver code
+if __name__ == "__main__":
+
+    dp = [[-1 for x in range(1000)]
+          for y in range(1000)]
+
+    n = len(str)
+    print("Total palindromic subsequence are :",
+          countPS(0, n - 1))
+"""
+"""
+from itertools import combinations
+msg='satishbabukatta'.lower()
+
+str2=[]
+lst=[]
+sub_strng=[]
+
+for i in range(1,len(msg)+1):
+    lst.append(list(combinations(msg, i)))
+    # print(lst)
+for i in lst:
+    for item in i:
+        sub_str=''.join(item)
+        sub_strng.append(sub_str)
+# print(sub_strng)
+# print(sub_strng)
+palindrome_stg=[]
+for i in sub_strng:
+    if i==i[::-1]:
+        palindrome_stg.append(i)
+print("Number of palindrome strings : ",len(set(palindrome_stg)))
+print(set(palindrome_stg))
+"""
+
+import itertools
+
+n = int(input("Enter size of string: "))
+string = input("Enter string: ")
+oplist = []
+finallst = []
+
+for i in range(1,n+1):
+    result = list(itertools.combinations(string,i))
+    # print(result)
+    oplist.append(result)
+#print(oplist)
+for element in oplist:
+    #print(element)
+    for item in element:
+        it = ''.join(item)
+        finallst.append(it)
+print(finallst)
+count = 0
+for element in finallst:
+    if element == element[::-1]:
+        count += 1
+        print(element)
+print("Total no. of palindrome subsequences are: ",count)
+
+"""
+
+
+
+
+
+
+
+
---
+++ _02_python_basics_selenium/_02_Datastructures.py
@@ -0,0 +1,61 @@
+"""
+Strings:
+-------
+Define String by using -------> ","  ','
+Ex--> "This is a string in double quotes"
+Ex--> 'This is a string in single quotes'
+"""
+x = "This is a string in double quotes"
+y = 'This is a string in single quotes'
+print("Enter the String : ",x)
+print("Enter the string : ",y)
+
+#indexing
+print(x[3])
+
+x = "This is a 'string' in double quotes"
+y = 'This is a "string" in single quotes'
+
+print("Enter the String : ",x)
+print("Enter the string : ",y)
+
+x = "This is a /'string'/ in double quotes"
+y = 'This is a /"string"/ in single quotes'
+
+print("Enter the String : ",x)
+print("Enter the string : ",y)
+
+y = """
+This is a multi line comment
+"""
+z = '''                   
+This is a multi line comment
+'''
+print("Enter the String : ",y)
+print("Enter the string : ",z)
+
+print("is" in z)
+print("is" not in z)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
